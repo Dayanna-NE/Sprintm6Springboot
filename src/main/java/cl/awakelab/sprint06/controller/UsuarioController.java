653470512f39c6b34690315b6ef82b1b82b631be
Dayhip Nuñez <dayhip.ti@gmail.com>
2023-07-13 01:31:25 -0400

    EditarUsuario v2

--- src/main/java/cl/awakelab/sprint06/controller/UsuarioController.java
+++ src/main/java/cl/awakelab/sprint06/controller/UsuarioController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -29,5 +30,14 @@ public class UsuarioController {
         objUsuarioService.crearUsuario(usuario);
         return "redirect:/usuario";
     }
+
+    @GetMapping("/editar/{idUsuario}")
+    public String mostrarFormularioEditarUsuario(@PathVariable int idUsuario, Model model){
+        Usuario usuarioQueEditaremos = objUsuarioService.buscarUsuarioId(idUsuario);
+        model.addAttribute("title","Editar Usuario");
+        model.addAttribute("usuarioHtml",usuarioQueEditaremos);
+        return "editarUsuario";
+    }
+
 
 }
