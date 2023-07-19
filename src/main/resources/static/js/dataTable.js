ec8d204c86019f53c866604a566e20e4a15dea95
Dayhip Nuñez <dayhip.ti@gmail.com>
2023-07-19 12:37:09 -0400

    datatable run

--- src/main/resources/static/js/dataTable.js
+++ src/main/resources/static/js/dataTable.js
@@ -1,3 +1,93 @@
 
- var table =new DataTable('#lista');
+//Pedir que inicie mi tabla con Datatable
+//var table =new DataTable('#lista');
 
+
+
+ $(document).ready(function() {
+  //Llamo a la tabla que modificaré
+  $('#lista').DataTable({
+   language: {
+    url: '//cdn.datatables.net/plug-ins/1.13.5/i18n/es-ES.json',
+   },
+   // Opciones de configuración de DataTables
+   "columnDefs": [
+       // Modificar la columna del salario usando la opción "render"
+    {
+     "targets": 1, // Índice de la columna, 1 = RUN
+     "render": function(data) {
+      // "data" es el valor original de la celda
+      // "type" es el tipo de renderizado (puede ser 'display', 'filter', 'sort' u 'type')
+      // "row" es el objeto de datos completo para la fila actual
+
+      // llamo a mi funcion para formatear el campo :D
+      return formatearRun(data);
+     }
+    }
+   ]
+  });
+ });
+
+
+
+//Funcion para formatear RUN
+ function formatearRun(data) {
+
+  var run = data;
+  var caracter;
+  var runInvertido = "";
+  var multiplicador = 1;
+  var resultado = 0;
+  var formatearARun = "";
+  for (let i = run.length - 1; i >= 0; i--) {
+   caracter = run.charAt(i);
+   runInvertido += caracter;
+   if (multiplicador < 7) {
+    multiplicador++;
+    resultado += multiplicador * caracter;
+
+   } else {
+    multiplicador = 2;
+    resultado += multiplicador * caracter;
+   }
+   if (run.length === 8) {
+    switch (i + 1) {
+     case 3:
+     case 6:
+      formatearARun = "." + caracter + formatearARun;
+      break;
+     default:
+      formatearARun = caracter + formatearARun;
+      break;
+    }
+   } else {
+    switch (i + 1) {
+     case 2:
+     case 5:
+      formatearARun = "." + caracter + formatearARun;
+      break;
+     default:
+      formatearARun = caracter + formatearARun;
+      break;
+    }
+
+   }
+  }
+  var resultadoTotal = resultado;
+  formatearARun = formatearARun + "-";
+  resultado = Math.floor(resultado / 11);
+  resultado = resultado * 11;
+  resultadoTotal = resultadoTotal - resultado;
+  switch (resultadoTotal) {
+   case 10:
+    formatearARun += "K";
+    break;
+   case 11:
+    formatearARun += "0";
+    break;
+   default:
+    formatearARun += resultadoTotal;
+    break;
+  }
+  return formatearARun;
+ }
--- src/main/resources/static/js/validaciones.js
+++ src/main/resources/static/js/validaciones.js
@@ -13,6 +13,79 @@ function controlarSoloNumero(idInput ='#run') {
         $(idInput).val( input.replace(/\D/g, ''));
     }
 }
+
+function codigoVerificador() {
+    $("#runTd").each(function (){
+        var run = $('#runTd').text();
+        var caracter;
+        var runInvertido = "";
+        var multiplicador=1;
+        var resultado=0;
+        var formatearARun="";
+        for (let i = run.length - 1; i >= 0; i--) {
+            caracter = run.charAt(i);
+            runInvertido += caracter;
+            if (multiplicador < 7) {
+                multiplicador++;
+                resultado += multiplicador * caracter;
+                console.log(multiplicador + " * " + caracter);
+
+            } else {
+                multiplicador = 2;
+                console.log(multiplicador + " * " + caracter);
+                resultado += multiplicador * caracter;
+            }
+            if (run.length === 8) {
+                switch (i+1) {
+                    case 3:
+                    case 6:
+                        formatearARun = "."+ caracter + formatearARun ;
+                        break;
+                    default:
+                        formatearARun = caracter + formatearARun;
+                        break;
+                }
+            } else {
+                switch (i+1) {
+                    case 2:
+                    case 5:
+                        formatearARun = "."+caracter + formatearARun ;
+                        break;
+                    default:
+                        formatearARun = caracter + formatearARun;
+                        break;
+                }
+                console.log(caracter);
+            }
+        }
+        var resultadoTotal = resultado;
+        formatearARun=formatearARun+"-";
+        console.log(runInvertido);
+        console.log("Suma totalo: "+resultado);
+        console.log("formateado: "+formatearARun);
+        resultado =  Math.floor(resultado/11);
+        console.log("resultado dividiendo /11: "+resultado);
+        resultado = resultado *11;
+        console.log("resultado multiplicando 11: "+resultado);
+        resultadoTotal = resultadoTotal - resultado;
+        console.log("resultadoTotal - resultado: "+resultadoTotal);
+        switch (resultadoTotal){
+            case 10:
+                formatearARun +="K";
+                break;
+            case 11:
+                formatearARun +="0";
+                break;
+            default:
+                formatearARun +=resultadoTotal;
+                break;
+        }
+        console.log("Run Completo: "+formatearARun);
+        $('#runTd').text(formatearARun);
+    });
+}
+
+
 $(document).ready(function() {
     // Asignar el evento input al input RUN
     $('#run').on('input', function() {
